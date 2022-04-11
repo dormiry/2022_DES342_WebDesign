d48729145a5b2ab8e344a431246a9c950480ec1d
dormiry <aamack9@gmail.com>
2022-04-11 14:56:16 -0700

    hw2

---
+++ homework/hw2/script.js
@@ -0,0 +1,16 @@
+// $('.animal-pic').css('border', '2px dotted black').css('padding', '40px')
+
+$('.one').click(function () {
+        $('.animal-pic').attr('src', 'images/3.jpg')
+    }
+)
+
+$('.two').click(function () {
+    $('.animal-pic').attr('src', 'images/2.jpg')
+}
+)
+
+$('.three').click(function () {
+    $('.animal-pic').attr('src', 'images/1.jpg')
+}
+)
