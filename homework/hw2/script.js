85e2cf7926fe36b7ba25110e998347bd0581429c
dormiry <aamack9@gmail.com>
2022-04-11 15:52:18 -0700

    hw2

--- homework/hw2/script.js
+++ homework/hw2/script.js
@@ -1,16 +1,47 @@
 // $('.animal-pic').css('border', '2px dotted black').css('padding', '40px')
 
 $('.one').click(function () {
-        $('.animal-pic').attr('src', 'images/3.jpg')
+        $('.pic').attr('src', 'images/1.jpg')
     }
 )
 
 $('.two').click(function () {
-    $('.animal-pic').attr('src', 'images/2.jpg')
+    $('.pic').attr('src', 'images/2.jpg')
 }
 )
 
 $('.three').click(function () {
-    $('.animal-pic').attr('src', 'images/1.jpg')
+    $('.pic').attr('src', 'images/3.jpg')
 }
 )
+
+$('.four').click(function () {
+    $('.pic').attr('src', 'images/4.jpg')
+}
+)
+
+$('.five').click(function () {
+    $('.pic').attr('src', 'images/5.jpg')
+}
+)
+
+$('.six').click(function () {
+    $('.pic').attr('src', 'images/6.jpg')
+}
+)
+
+$('.seven').click(function () {
+    $('.pic').attr('src', 'images/7.jpg')
+}
+)
+
+$('.eight').click(function () {
+    $('.pic').attr('src', 'images/8.jpg')
+}
+)
+
+$('.nine').click(function () {
+    $('.pic').attr('src', 'images/9.jpg')
+}
+)
+
